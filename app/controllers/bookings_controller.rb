648efd9704547e68b2d3b58840b667a30a10f696
Desmond Yentumi <yentumid7@gmail.com>
2024-06-29 23:58:04 +0000

    Added bookings and flights controllers.

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,15 @@
+class FlightsController < ApplicationController
+
+    def index
+        if params[:flight]
+            @flights = Flight.where(departure_airport_id: params[:flight][:departure_airport_id],
+                                arrival_airport_id: params[:flight][:arrival_airport_id],)
+                                .where("DATE(start_datetime) = ?", params[:start_datetime])
+        else
+           @flights = Flight.all
+        end
+  
+    end
+  
+end
+  
---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,31 @@
+class BookingsController < ApplicationController
+    def new
+      @flight = Flight.find(params[:flight_id])
+      @booking = @flight.bookings.build
+      passengers = params[:passengers].to_i
+      passengers.times { @booking.passengers.build }
+    end
+  
+    def create
+      @flight = Flight.find(params[:booking][:flight_id])
+      @booking = @flight.bookings.build(booking_params)
+      if @booking.save
+        @booking.passengers.each do |passenger|
+          PassengerMailer.with(passenger: passenger, booking: @booking).ticket_email.deliver_now
+        end
+        redirect_to @booking
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  
+    def show
+      @booking = Booking.find(params[:id])
+    end
+  
+    private
+    def booking_params
+      params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+    end
+  end
+  
