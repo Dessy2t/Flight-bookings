97f3a5bed1f27d854ed10f4e4233f80c5ac6998b
Desmond Yentumi <yentumid7@gmail.com>
2024-06-30 01:00:25 +0000

    edited controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,31 +1,14 @@
-class BookingsController < ApplicationController
-    def new
-      @flight = Flight.find(params[:flight_id])
-      @booking = @flight.bookings.build
-      passengers = params[:passengers].to_i
-      passengers.times { @booking.passengers.build }
+class FlightsController < ApplicationController
+
+  def index
+    if params[:flight]
+          @flights = Flight.where(departure_airport_id: params[:flight][:departure_airport_id],
+                              arrival_airport_id: params[:flight][:arrival_airport_id],)
+                              .where("DATE(start_datetime) = ?", params[:start_datetime])
+    else
+         @flights = Flight.all
     end
-  
-    def create
-      @flight = Flight.find(params[:booking][:flight_id])
-      @booking = @flight.bookings.build(booking_params)
-      if @booking.save
-        @booking.passengers.each do |passenger|
-          PassengerMailer.with(passenger: passenger, booking: @booking).ticket_email.deliver_now
-        end
-        redirect_to @booking
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-  
-    def show
-      @booking = Booking.find(params[:id])
-    end
-  
-    private
-    def booking_params
-      params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
-    end
+
   end
-  
+
+end
